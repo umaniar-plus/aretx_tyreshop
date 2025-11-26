28a90276634002c0f40ad1ccbafad35966b5de5c
Areterix <mmalek@areterix.com>
2025-11-26 17:49:13 +0530

    update

--- aretx_vehicle/models/tyre_service_reminder.py
+++ aretx_vehicle/models/tyre_service_reminder.py
@@ -769,21 +769,21 @@ class AccountMove(models.Model):
                     "to": clean_phone,
                     "type": "template",
                     "template": {
-                        "name": "reminder_service",
+                        "name": "payment_overdue",
                         "language": {"code": "en"},
                         "components": [
-                            # {
-                            #     "type": "header",
-                            #     "parameters": [
-                            #         {
-                            #             "type": "document",
-                            #             "document": {
-                            #                 "link": pdf_url,
-                            #                 "filename": invoice.name
-                            #             }
-                            #         }
-                            #     ]
-                            # },
+                            {
+                                "type": "header",
+                                "parameters": [
+                                    {
+                                        "type": "document",
+                                        "document": {
+                                            "link": pdf_url,
+                                            "filename": invoice.name
+                                        }
+                                    }
+                                ]
+                            },
                             {
                                 "type": "body",
                                 "parameters": [
